a131f9c94268b5234e538d63d3f080153e8b11ca
Rubaiyat Khondaker <rubaiyat.khondaker@gmail.com>
2018-07-15 12:05:05 +0100

    Added documentation for all functions.

--- sat_logic.py
+++ sat_logic.py
@@ -2,12 +2,22 @@ import pdb
 
 
 def logic_rules(logic_array: list) -> list:
-    if logic_array[0] == "AND" and any(
-            element is False for element in logic_array):
-        return [False]
-    elif logic_array[0] == "OR" and any(
+    """
+    Simplify logic array based on AND and OR rules.
+
+    Immediately return [True] or [False] for these situations:
+
+        1: Operator is "OR", and 'True' is in the array -> [True].
+        2: Operator is "AND", and 'False' is in the array -> [False].
+
+    Otherwise, loop through list, and apply logic_conversion on each element.
+    """
+    if logic_array[0] == "OR" and any(
             element is True for element in logic_array):
         return [True]
+    elif logic_array[0] == "AND" and any(
+            element is False for element in logic_array):
+        return [False]
     else:
         return [
             logic_conversion(element) for element in logic_array
@@ -15,6 +25,15 @@ def logic_rules(logic_array: list) -> list:
 
 
 def logic_conversion(element):
+    """Convert the element into the appropriate form for logic_rules.
+
+    Apply logic_rules recursively if the element is itself a logic array.
+
+    If the element is a boolean, it is not added, as either:
+        1: The operator is "OR" and the element is 'False'
+        2: The operator is "AND" and the element is 'True'
+    The other two combinations are already covered in logic_rules.
+    """
     if isinstance(element, list):
         return logic_rules(element)
     elif not isinstance(element, bool):
@@ -24,12 +43,14 @@ def logic_conversion(element):
 
 
 def set_variable(logic_array: list, var_num: int, value: bool) -> list:
+    """Set a truth value to a certain variable in the logic array."""
     return [
         set_variable_converter(element, var_num, value)
         for element in logic_array]
 
 
 def set_variable_converter(element, var_num: int, value: bool):
+    """Convert element into the appropriate form for set_variable."""
     if isinstance(element, list):
         return set_variable(element, var_num, value)
     elif element == var_num:
