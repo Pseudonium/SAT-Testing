965956083162af2c648417f21cf78b44a999e48a
Rubaiyat Khondaker <rubaiyat.khondaker@gmail.com>
2018-07-15 11:32:17 +0100

    Simplified functions to list comps, added annotations.

--- sat_logic.py
+++ sat_logic.py
@@ -1,43 +1,49 @@
 import pdb
 
 
-def true_false_rules(logic_array):
-    new_array = []
-    # pdb.set_trace()
+def logic_rules(logic_array: list) -> list:
     if logic_array[0] == "AND" and any(
             element is False for element in logic_array):
-        new_array.append(False)
-        return new_array
+        return [False]
     elif logic_array[0] == "OR" and any(
             element is True for element in logic_array):
-        new_array.append(True)
-        return new_array
+        return [True]
     else:
-        for element in logic_array:
-            if isinstance(element, list):
-                new_array.append(true_false_rules(element))
-            elif logic_array[0] == "AND" and element is not True:
-                new_array.append(element)
-            elif logic_array[0] == "OR" and element is not False:
-                new_array.append(element)
-    return new_array
+        return [
+            logic_conversion(element) for element in logic_array
+            if logic_conversion(element)]
 
 
-def set_variable(logic_array, var_num, value):
-    new_array = []
-    for element in logic_array:
-        if isinstance(element, list):
-            new_array.append(set_variable(element, var_num, value))
-        elif element == var_num:
-            new_array.append(value)
-        elif element == var_num * -1:
-            new_array.append(not value)
-        else:
-            new_array.append(element)
-    return new_array
+def logic_conversion(element):
+    if isinstance(element, list):
+        return logic_rules(element)
+    elif not isinstance(element, bool):
+        return element
+    else:
+        return False
 
 
-print(true_false_rules(["AND", False, 2, -3]))
-print(set_variable(
+def set_variable(logic_array: list, var_num: int, value: bool) -> list:
+    return [
+        set_variable_converter(element, var_num, value)
+        for element in logic_array]
+
+
+def set_variable_converter(element, var_num: int, value: bool):
+    if isinstance(element, list):
+        return set_variable(element, var_num, value)
+    elif element == var_num:
+        return value
+    elif element == var_num * -1:
+        return not value
+    else:
+        return element
+
+
+"""
+x = set_variable(
     ["AND", ["OR", ["AND", 1, 7], ["AND", 2, -7], 3],
-     ["OR", ["AND", 4, 7], ["AND", 5, -7], 6]], 7, False))
+     ["OR", ["AND", 4, 7], ["AND", 5, -7], 6]], 7, True)
+print(x)
+print(logic_rules(x))
+"""
