61e57d1109f4b03cc5946ec96bcdd56bc7501230
Rubaiyat Khondaker <rubaiyat.khondaker@gmail.com>
2018-07-27 19:26:50 +0100

    Added dimacs_parser function, module-level

--- rSAT.py
+++ rSAT.py
@@ -1,27 +1,79 @@
 import pdb
 import os
+import collections
 
 
 class Error(Exception):
     pass
 
 
+class FormatError(Error):
+    pass
+
+
+def cnf_parser(dimacs_line):
+    line = dimacs_line
+    line = line.split(" ")
+    while '' in line:
+        line.remove('')
+    # 0 is always at the end of the line, so we remove it
+    end = line.pop()
+    if end != '0\n':
+        print(line)
+        raise FormatError("0 must terminate all cnf lines.")
+    line = [int(element) for element in line[:]]
+    line.insert(0, "OR")
+    return line
+
+
 def dimacs_parser(dimacs_filepath):
-    raise NotImplementedError
+    try:
+        f = open(dimacs_filepath)
+    except IOError:
+        raise IOError("File either corrupted or not found.")
+    else:
+        Parameters = collections.namedtuple(
+            'Parameters', ['logic_list', 'var_num', 'clause_num'])
+        with f:
+            logic_list = ["AND"]
+            cnf_reached = False
+            for line in f:
+                if cnf_reached:
+                    if line[0] == "%":
+                        break
+                    line = cnf_parser(line)
+                    logic_list.append(LogicStatement(line))
+                elif line[0] == "p":
+                    cnf_reached = True
+                    line = line.split(" ")
+                    while '' in line:
+                        line.remove('')
+                    var_num = line[2]
+                    clause_num = line[3]
+        return Parameters(logic_list, var_num, clause_num)
+
+    # raise NotImplementedError
 
 
 class LogicStatement:
 
-    def __init__(self, logic_list: list):
+    def __init__(self, logic_list: list, dimacs_dict: dict = None):
         self.operator = logic_list[0]
         self.contents = [
             LogicStatement(element) if isinstance(element, list)
             else element for element in logic_list[1:]
         ]
+        if dimacs_dict is not None:
+            for key, value in dimacs_dict.items():
+                setattr(self, key, value)
 
     @classmethod
     def from_dimacs(cls, dimacs_filepath: str):
-        return cls(dimacs_parser(dimacs_filepath))
+        parsed_file = dimacs_parser(dimacs_filepath)
+        return cls(parsed_file.logic_list, {
+            'var_num': parsed_file.var_num,
+            'clause_num': parsed_file.clause_num
+        })
 
 
 x = LogicStatement(["AND",
@@ -30,3 +82,4 @@ x = LogicStatement(["AND",
                    )
 print(x)
 y = LogicStatement.from_dimacs("../rSAT-instances/uf20-01.cnf")
+print(y)
