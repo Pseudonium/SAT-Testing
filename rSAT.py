68d9fd474ad4381df88f24bff71138fe8e9a35f4
Rubaiyat Khondaker <rubaiyat.khondaker@gmail.com>
2018-07-27 19:45:17 +0100

    Added str and repr methods, and moved testing to a __name__ = "__main__"

--- rSAT.py
+++ rSAT.py
@@ -19,7 +19,8 @@ def cnf_parser(dimacs_line):
     # 0 is always at the end of the line, so we remove it
     end = line.pop()
     if end != '0\n':
-        print(line)
+        line.append(end)
+        print("Error line: ", line)
         raise FormatError("0 must terminate all cnf lines.")
     line = [int(element) for element in line[:]]
     line.insert(0, "OR")
@@ -66,20 +67,43 @@ class LogicStatement:
         if dimacs_dict is not None:
             for key, value in dimacs_dict.items():
                 setattr(self, key, value)
+            self.attr_dict = dimacs_dict
+
+    def __repr__(self):
+        if hasattr(self, 'attr_dict'):
+            return "LogicStatement({}, {})".format(
+                self.display(), self.attr_dict)
+        else:
+            return "LogicStatement({})".format(self.display())
+
+    def __str__(self):
+        return str(self.display())
+
+    def display(self):
+        return [self.operator] + [
+            element.display() if isinstance(element, LogicStatement)
+            else element for element in self.contents
+        ]
 
     @classmethod
     def from_dimacs(cls, dimacs_filepath: str):
         parsed_file = dimacs_parser(dimacs_filepath)
-        return cls(parsed_file.logic_list, {
-            'var_num': parsed_file.var_num,
-            'clause_num': parsed_file.clause_num
-        })
+        return cls(
+            parsed_file.logic_list, {
+                'var_num': parsed_file.var_num,
+                'clause_num': parsed_file.clause_num
+            }
+        )
 
 
-x = LogicStatement(["AND",
-                    ["OR", ["AND", 1, 7], ["AND", 2, -7], 3],
-                    ["OR", ["AND", 4, 7], ["AND", 5, -7], 6]]
-                   )
-print(x)
-y = LogicStatement.from_dimacs("../rSAT-instances/uf20-01.cnf")
-print(y)
+if __name__ == "__main__":
+
+    x = LogicStatement(["AND",
+                        ["OR", ["AND", 1, 7], ["AND", 2, -7], 3],
+                        ["OR", ["AND", 4, 7], ["AND", 5, -7], 6]]
+                       )
+    print(x)
+    print(repr(x))
+    y = LogicStatement.from_dimacs("../rSAT-instances/uf20-01.cnf")
+    print(y)
+    print(repr(y))
