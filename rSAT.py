840e363e8cc70bc5baa07e05068fccae27a48396
Rubaiyat Khondaker <rubaiyat.khondaker@gmail.com>
2018-07-27 17:47:27 +0100

    Split __init__ to logic list and dimacs constructor

--- rSAT.py
+++ rSAT.py
@@ -6,36 +6,27 @@ class Error(Exception):
     pass
 
 
-class KeywordNumError(Error, TypeError):
-    pass
+def dimacs_parser(dimacs_filepath):
+    raise NotImplementedError
 
 
 class LogicStatement:
-    def __init__(self, dimacs_filepath: str = None, logic_list: list = None):
-        if dimacs_filepath and logic_list:
-            raise KeywordNumError(
-                "Cannot pass both filepath and list as arguments")
-        elif dimacs_filepath:
-            self.operator = "AND"
-            self.contents = []
-            self.dimacs_parser(dimacs_filepath)
-        elif logic_list:
-            self.operator = logic_list[0]
-            self.contents = [
-                LogicStatement(logic_list=element)
-                if isinstance(element, list)
-                else element for element in logic_list
 
-    def __str__(self):
+    def __init__(self, logic_list: list):
+        self.operator = logic_list[0]
+        self.contents = [
+            LogicStatement(element) if isinstance(element, list)
+            else element for element in logic_list[1:]
+        ]
+
+    @classmethod
+    def from_dimacs(cls, dimacs_filepath: str):
+        return cls(dimacs_parser(dimacs_filepath))
 
 
-    def dimacs_parser(self, dimacs_filepath):
-        raise NotImplementedError
-
-
-x= LogicStatement(
-    logic_list=["AND",
-                ["OR", ["AND", 1, 7], ["AND", 2, -7], 3],
-                ["OR", ["AND", 4, 7], ["AND", 5, -7], 6]]
-)
+x = LogicStatement(["AND",
+                    ["OR", ["AND", 1, 7], ["AND", 2, -7], 3],
+                    ["OR", ["AND", 4, 7], ["AND", 5, -7], 6]]
+                   )
 print(x)
+y = LogicStatement.from_dimacs("../rSAT-instances/uf20-01.cnf")
